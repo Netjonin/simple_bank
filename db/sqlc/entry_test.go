ae4dc8e0d73f52763314364692006f4ba954632e
netjonin <pencilpepeyo@yahoo.com>
2024-02-03 22:36:39 +0100

    Create entry test added

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -1,12 +1,37 @@
 package db
 
-// import (
-// 	"context"
-// 	"testing"
-// )
+import (
+	"context"
+	"testing"
+	"time"
 
+	"github.com/netjonin/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
 
+func createRandomEntry(t *testing.T) Entry {
+	Ids, err := testQueries.ListAccountIDs(context.Background())
+	require.NoError(t, err)
 
-// // func createRandomEntry(t *testing.T) {
-// // 	accounts, err := testQueries.ListAccounts(context.Background(), arg)
-// // }
+	Id := util.RandomInt(Ids[0], Ids[len(Ids) - 1])
+	arg := CreateEntryParams {
+		AccountID: Id,
+		Amount: util.RandomMoney(),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.AccountID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t)
+}
+
